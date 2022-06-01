021a60e0abf162959a57982bf652b66da32c30df
Gabriel Martins <gabtec@sapo.pt>
2022-06-01 15:26:50 +0100

    Fix line space missing when color specified

--- assets/banner.js
+++ assets/banner.js
@@ -1,16 +1,18 @@
-const banner = "\n" + 
-" _____         _            _        _      \n" +
-"|  __ \\       | |          (_)      | |     \n" +
-"| |  \\/  __ _ | |__   _ __  _   ___ | |     \n" +
-"| | __  / _` || '_ \\ | '__|| | / _ \\| |     \n" +
-"| |_\\ \\| (_| || |_) || |   | ||  __/| |     \n" +
-" \\____/ \\__,_||_.__/ |_|   |_| \\___||_|";
+require("colors"); 
 
-   function print(suffix) {
-     const append = suffix ? suffix : "";
-     const fullBanner = banner + " " + append + "\n";
-     console.log(fullBanner);
-   };
-   
-   module.exports = { print };
-   
+const banner = "\n" + 
+" _____         _      _                 _____                _      \n" +
+"|  __ \\       | |    | |               |_   _|              | |     \n" +
+"| |  \\/  __ _ | |__  | |_   ___   ___    | |    ___    ___  | |     \n" +
+"| | __  / _` || '_ \\ | __| / _ \\ / __|   | |   / _ \\  / _ \\ | |     \n" +
+"| |_\\ \\| (_| || |_) || |_ |  __/| (__    | |  | (_) || (_) || |     \n" +
+" \\____/ \\__,_||_.__/  \\__| \\___| \\___|   \\_/   \\___/  \\___/ |_|";
+
+    function print(suffix) {
+      const append = suffix ? suffix : "";
+      const fullBanner = banner + " " + append + "\n";
+      console.log(fullBanner.red);
+    };
+    
+    module.exports = { print };
+    
